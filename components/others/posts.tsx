76b6888717d59b574183c2a443da9561a8245015
enochndika <enochbeloved@gmail.com>
2021-05-15 06:12:42 +0100

    fix undefined post image

--- components/others/posts.tsx
+++ components/others/posts.tsx
@@ -23,42 +23,56 @@ export default function Post({
   return (
     <Row className="mt-2">
       {post &&
-        post.map((item) => (
-          <div className={mainColClass} key={item.id}>
-            <Row>
-              <div className={firstColClass} key={item.id}>
-                <Link href={`/posts/${item.slug || item.post?.slug}`}>
-                  <a>
-                    <Image
-                      height={900}
-                      width={1500}
-                      src={item.image[0] || item.post?.image[0]}
-                      className="block w-full cursor-pointer"
-                      alt={item.title | item.post?.title}
-                    />
-                  </a>
-                </Link>
-              </div>
-              <div className={secondColClass}>
-                <div className="mb-2.5 text-2xl text-gray-700 font-medium dark:text-white">
-                  {item.title || item.post.title}
+        post.map((item) => {
+          console.log(item.post.image);
+          return (
+            <div className={mainColClass} key={item.id}>
+              <Row>
+                <div className={firstColClass} key={item.id}>
+                  <Link href={`/posts/${item.slug || item.post?.slug}`}>
+                    <a>
+                      {item.image && item?.image?.length > 0 ? (
+                        <Image
+                          height={900}
+                          width={1500}
+                          src={item?.image[0]}
+                          className="block w-full cursor-pointer"
+                          alt={item.title || item.post?.title}
+                        />
+                      ) : (
+                        <Image
+                          height={900}
+                          width={1500}
+                          src={item.post?.image[0]}
+                          className="block w-full cursor-pointer"
+                          alt={item.title || item.post?.title}
+                        />
+                      )}
+                    </a>
+                  </Link>
                 </div>
-                <p className="mb-4 text-gray-600 dark:text-grayer text-small">
-                  {item.description?.slice(0, 130) ||
-                    item.post?.description?.slice(0, 130)}
-                </p>
-                <PostDetails
-                  author={item.user?.fullName || item.post?.user?.fullName}
-                  category={
-                    item.posts_category?.name || item.post?.posts_category?.name
-                  }
-                  date={item.createdAt || item.post?.createdAt}
-                  readTime={item.read_time || item.post?.read_time}
-                />
-              </div>
-            </Row>
-          </div>
-        ))}
+                <div className={secondColClass}>
+                  <div className="mb-2.5 text-2xl text-gray-700 font-medium dark:text-white">
+                    {item.title || item.post.title}
+                  </div>
+                  <p className="mb-4 text-gray-600 dark:text-grayer text-small">
+                    {item.description?.slice(0, 130) ||
+                      item.post?.description?.slice(0, 130)}
+                  </p>
+                  <PostDetails
+                    author={item.user?.fullName || item.post?.user?.fullName}
+                    category={
+                      item.posts_category?.name ||
+                      item.post?.posts_category?.name
+                    }
+                    date={item.createdAt || item.post?.createdAt}
+                    readTime={item.read_time || item.post?.read_time}
+                  />
+                </div>
+              </Row>
+            </div>
+          );
+        })}
       {children}
     </Row>
   );
