3723d93ae55c2b164365bbcadbecf1ea65a2cf65
enochndika <enochbeloved@gmail.com>
2021-10-08 18:58:54 +0100

    update cookie expiration date and .env file

--- src/config/cookies.tsx
+++ src/config/cookies.tsx
@@ -1,9 +1,15 @@
 import cookie from 'js-cookie';
 
 const setCookie = (key: string, value: string) => {
+  const today = new Date();
+  const expirationDate = new Date(today);
+
+  expirationDate.setDate(today.getDate() + 30);
+  const expireIn = parseInt(String(expirationDate.getTime() / 1000), 10);
+
   if (typeof window !== 'undefined') {
     cookie.set(key, value, {
-      expires: 2,
+      expires: expireIn,
       path: '/',
     });
   }
